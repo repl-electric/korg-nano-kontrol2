5eacc03a1c9a693fdcf38eaf60c89d0fd5942f70
Joseph Wilk <joe@josephwilk.net>
2013-10-19 15:23:35 +0200

    words

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject korg-nano-kontrol2  "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Using NanoKontrol2 with Overtone"
+  :url "https://github.com/the-stars-my-destination/korg-nano-kontrol2"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [overtone "0.9.0-SNAPSHOT"]])
